a0a9674ff61d90eb4b7eff299dcbe5b6be51afe4
Arthur Reimus <arthurreimus1623@gmail.com>
2022-04-19 23:44:00 +0800

    Finished the html for the form

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-  const pass1 = document.querySelector("#pass");
-  const pass2 = document.querySelector("#confirm-pass");
-  const errorMsg = document.querySelector(".pass-error-msg");
-  if (pass1 !== pass2) {
-    e.preventDefault();
-    pass1.classList.add("error");
-    pass2.classList.add("error");
-    errorMsg.style.display = "block";
+function matchPassword() {
+  const pw1 = document.getElementById("password-one").value;
+  const pw2 = document.getElementById("password-two").value;
+
+  if (pw1 != pw2) {
+    alert("Passwords did not match");
+  } else {
+    alert("Password created successfully");
   }
-});
+}
